20503fc85a0c3344cce8b31adeceb22b21b2a31a
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-04 16:49:00 -0600

    get input (customer age) from command line handle errors- invalid numbers/negative age set age categories

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Please enter customer age")
+	}
+
+	age := os.Args[1]
+
+	if age, err := strconv.Atoi(age); err != nil || age < 1 {
+		fmt.Printf("%v is not a vlaid age. Please enter customer age", age)
+		return
+	} else if age >= 17 {
+		fmt.Println("R-Rated")
+	} else if age < 17 && age >= 13 {
+		fmt.Println("PG-13")
+	} else if age < 13 {
+		fmt.Println("PG-Rated")
+	}
 
 }
